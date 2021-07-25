59222f480848b70291f276526b25e21123aeb17d
The220th <The220th@gmail.com>
2021-07-25 18:30:51 +0300

    second

--- ViewerYUV.java
+++ ViewerYUV.java
@@ -3,28 +3,45 @@ import java.util.*;
 import java.io.*;
 import java.awt.*;
 import java.io.IOException;
-
+import java.awt.image.BufferedImage;
 import javax.swing.*;
+import javax.swing.event.*;
 
 public class ViewerYUV
 {
     private static ViewerYUV picFrame;
     private static String pathToFile;
+    private static String pathToFile2;
     private static int W;
     private static int H;
+    private static int frameNUM;
+    private static int frameMAX;
     private static String mode;
     private static int[] rgbImage;
+    private static int[] rgbImage2;
 
     private JFrame mainFrame;
+    private JPanel picomsPanel;
     private sComponent picom;
+    private sComponent picom_2;
+
+    private JButton nextButton;
+    private JTextField frameText;
+    private JSlider frameSlider;
+    private JButton prevButton;
+    private JToolBar toolBar;
 
     public static void main(String[] args)
     {
+        pathToFile2 = null;
+        rgbImage2 = null;
         /*frags:
         first is pathToFile
         -w
         -h
         -f 420 or 444
+        -num frameNum
+        -d second.yuv
         */
         if(args == null || args.length == 0)
         {
@@ -58,6 +75,11 @@ public class ViewerYUV
                     }
                     break;
                 }
+                case "-d":
+                {
+                    pathToFile2 = args[i];
+                    break;
+                }
                 case "-?":
                 {
                     System.out.println(help());
@@ -74,12 +96,18 @@ public class ViewerYUV
             }
         }
 
-        int[] yuvImage = readImage();
-
-        rgbImage = yuv2rgb(yuvImage, W, H);
-
         picFrame = new ViewerYUV();
         picFrame.show();
+    }
+
+    private void updateFrame(int frameNUM)
+    {
+        int[] yuvImage = readImage(pathToFile, frameNUM);
+
+        rgbImage = yuv2rgb(yuvImage, W, H);
+        //saveImage(rgbImage);
+        if(pathToFile2 != null)
+        	rgbImage2 = yuv2rgb(readImage(pathToFile2, frameNUM), W, H);
     }
 
     private static String help()
@@ -89,10 +117,11 @@ public class ViewerYUV
         res += "\t-w is width\n";
         res += "\t-h is height\n";
         res += "\t-f is format 420 or 444\n";
+        res += "\t-d second yuv-video\n";
         res += "\t-? is help\n\n";
 
         res += "For example: \n";
-        res += "> java ViewerYUV image.yuv -w 640 -h 480 -f 444\n\n";
+        res += "> java ViewerYUV image.yuv -w 640 -h 480 -f 444 -d secondYUVvideo.yuv\n\n";
 
         res += "Structure of the yuv-file if f=444:\n";
         res += "Suppose there is a 3x2 pixel image. That is, the width is 3 pixels, and the height is 2 pixels: \n";
@@ -108,33 +137,97 @@ public class ViewerYUV
     
     private void show()
     {
+        picom_2 = null;
+        frameMAX = -1;
+        frameNUM = 1;
+    	updateFrame(frameNUM);
+
         mainFrame = new JFrame("yuv image");
         //mainFrame.setSize(W, H);
         mainFrame.setLocation(50, 51);
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         picom = new sComponent(rgbImage, W, H);
+        if(pathToFile2 != null)
+        	picom_2 = new sComponent(rgbImage2, W, H);
 
-        mainFrame.add(picom);
+        nextButton = new JButton(">>");
+        nextButton.setToolTipText("Next frame");
+        nextButton.addActionListener(event -> 
+        {
+        	frameNUM = frameNUM+1>frameMAX?frameNUM:frameNUM+1;
+        	updateFrame(frameNUM);
+        	picom.update(rgbImage);
+        	if(picom_2 != null)
+        		picom_2.update(rgbImage2);
+        	frameText.setText(frameNUM + "/" + frameMAX);
+        	frameSlider.setValue(frameNUM);
+        });
+
+        prevButton = new JButton("<<");
+        prevButton.setToolTipText("Previous frame");
+        prevButton.addActionListener(event -> 
+        {
+        	frameNUM = frameNUM-1<1?frameNUM:frameNUM-1;
+        	updateFrame(frameNUM);
+        	picom.update(rgbImage);
+        	if(picom_2 != null)
+        		picom_2.update(rgbImage2);
+        	frameText.setText(frameNUM + "/" + frameMAX);
+        	frameSlider.setValue(frameNUM);
+        });
+
+        frameText = new JTextField(frameNUM + "/" + frameMAX, 25);
+        frameText.setEditable(false);
+
+        frameSlider = new JSlider(JSlider.HORIZONTAL, 1, frameMAX<2?frameMAX+1:frameMAX, 1);
+        frameSlider.addChangeListener(e -> 
+        {
+        	JSlider source = (JSlider)e.getSource();
+        	int v = (int)source.getValue();
+        	//System.out.println("==========" + v);
+        	frameNUM = v;
+        	//updateFrame(frameNUM);
+        	//picom.update(rgbImage);
+        	//if(picom_2 != null)
+        	//	picom_2.update(rgbImage2);
+        	frameText.setText(frameNUM + "/" + frameMAX);
+       	});
+
+		toolBar = new JToolBar("Tool bar");
+		toolBar.add(prevButton);
+		toolBar.add(frameText);
+		toolBar.add(frameSlider);
+		toolBar.add(nextButton);
+
+		mainFrame.setLayout(new BorderLayout());
+		mainFrame.add(toolBar, BorderLayout.NORTH);
+
+		picomsPanel = new JPanel();
+		picomsPanel.add(picom);
+		if(picom_2 != null)
+			picomsPanel.add(picom_2);
+        mainFrame.add(picomsPanel, BorderLayout.CENTER);
 
         mainFrame.pack();
         mainFrame.setVisible(true);
     }
 
-    private static int[] readImage()
+    private static int[] readImage(String path, int frameNum)
     {
         byte[] res = null;
-        try(FileInputStream fin = new FileInputStream(pathToFile))
+        try(FileInputStream fin = new FileInputStream(path))
         {
-            for(int frames_number = 0; frames_number < 1; ++frames_number)
-            {
-                if(mode.equals("444"))
-                    res = new byte[W*H*3];
-                else if(mode.equals("420"))
-                    res = new byte[(W*H*3)/2];
+            if(mode.equals("444"))
+                res = new byte[W*H*3];
+            else if(mode.equals("420"))
+                res = new byte[(W*H*3)/2];
 
-                fin.read(res, 0, res.length);
-            }
+            if(frameMAX < 0)
+            	frameMAX = fin.available() / res.length;
+
+            fin.skip((frameNum-1)*(res.length));
+            fin.read(res, 0, res.length);
         }
         catch(IOException ex)
         {
@@ -146,6 +239,21 @@ public class ViewerYUV
         if(mode.equals("420"))
             resInt = i420_to_i444(resInt, W, H);
         return resInt;
+    }
+
+    private static void saveImage(int[] img)
+    {
+        byte[] res = new byte[img.length];
+        for(int i = 0; i < res.length; ++i)
+        	res[i] = signedByte(img[i]);
+        try(FileOutputStream fos = new FileOutputStream("out"))
+        {
+            fos.write(res, 0, res.length);
+        }
+        catch(IOException ex)
+        {
+            ex.printStackTrace();
+        }
     }
 
     private static int[] i420_to_i444(int[] frame, int width, int height)
@@ -197,6 +305,20 @@ public class ViewerYUV
         return (x&((byte)(-128)))==0?(int)x:(int)x+256;
     }
 
+    /**
+     * Переводит unsigned byte, в виде int, в byte 
+     * 
+     * @param x - byte, где восьмой бит единица
+     * @return byte со знаком
+     * @throws IllegalArgumentException, если 0 > x или x >= 256
+     */
+    public static byte signedByte(int x) throws IllegalArgumentException
+    {
+        if(x < 0 || x >= 256)
+            throw new IllegalArgumentException("Byte can be only [0; 255], you gave: " + x);
+        return x < 128?(byte)x:(byte)(x-256);
+    }
+
     private static int[] yuv2rgb(int[] yuv, int width, int height)
     {
         int[] tmp = new int[3*width*height];
@@ -242,9 +364,9 @@ public class ViewerYUV
             if(tmp[k + red] > 255 || tmp[k + green] > 255 || tmp[k + blue] > 255
             || tmp[k + red] < 0 || tmp[k + green] < 0 || tmp[k + blue] < 0)
             {
-                System.out.print("Problem with pixel " + k + ": ");
-                System.out.print("yuv={" + yuv[k + red] + " " + yuv[k + green] + " " + yuv[k + blue] + "} <=> ");
-                System.out.println("rgb={" + tmp[k + red] + " " + tmp[k + green] + " " + tmp[k + blue] + "}");
+                //System.out.print("Problem with pixel " + k + ": ");
+                //System.out.print("yuv={" + yuv[k + red] + " " + yuv[k + green] + " " + yuv[k + blue] + "} <=> ");
+                //System.out.println("rgb={" + tmp[k + red] + " " + tmp[k + green] + " " + tmp[k + blue] + "}");
                 tmp[k + red] = tmp[k + red]>255?255:tmp[k + red];
                 tmp[k + green] = tmp[k + green]>255?255:tmp[k + green];
                 tmp[k + blue] = tmp[k + blue]>255?255:tmp[k + blue];
@@ -270,12 +392,27 @@ public class ViewerYUV
             DEFAULT_WIDTH = W;
             DEFAULT_HEIGHT = H;
         }
+
+        public void update(int[] pic)
+        {
+            this.pic = pic;
+            SwingUtilities.updateComponentTreeUI(mainFrame);
+            /*
+			mainFrame.invalidate();
+			mainFrame.validate();
+			mainFrame.repaint();
+            */
+        }
     
         public void paintComponent(Graphics gOld)
         {
-            Graphics2D g = (Graphics2D)gOld;
+        	int[] lpic = pic;
             int w = DEFAULT_WIDTH;
             int h = DEFAULT_HEIGHT;
+            Graphics2D gMain = (Graphics2D)gOld;
+            BufferedImage bufferedImage = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+            Graphics2D g = (Graphics2D)bufferedImage.getGraphics();
+
             int red = 0;
             int green = w * h;
             int blue = w * h * 2;
@@ -286,9 +423,11 @@ public class ViewerYUV
             {
                 x = i % w;
                 y = i / w;
-                c = new Color(pic[red+i], pic[green+i], pic[blue+i]);
+                c = new Color(lpic[red+i], lpic[green+i], lpic[blue+i]);
                 paintBlock(x, y, c, g);
             }
+
+            gMain.drawImage(bufferedImage, 0, 0, null);
         }
     
         public Dimension getPreferredSize()
